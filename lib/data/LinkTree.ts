67fa47fa56dd3fafb0531cd4561d0a635aad7c3e
Jorge Marcos PC <desarrolladorjorgem@gmail.com>
2023-03-04 20:38:41 +0100

    feat: Added extra files on icons, links, analytics and hooks

---
+++ lib/data/LinkTree.ts
@@ -0,0 +1,15 @@
+const linkTree = [
+  {
+    name: 'Portfolio',
+    href: '/',
+  },
+  {
+    name: 'Resume',
+    href: '/cv_jorge_marcos.pdf',
+  },
+  {
+    name: 'Github',
+    href: 'https://github.com',
+  },
+];
+export default linkTree;
---
+++ lib/data/socials.ts
@@ -0,0 +1,23 @@
+const socials = [
+  {
+    name: 'Github',
+    href: 'https://github.com/ItsJorgeMarcos',
+  },
+  {
+    name: 'linkedIn',
+    href: 'https://www.linkedin.com/in/jorge-marcos-carrero/',
+  },
+  {
+    name: 'twitter',
+    href: 'https://twitter.com',
+  },
+  {
+    name: 'Mail',
+    href: 'mailto:desarrolladorjorgem@gmail.com',
+  },
+  {
+    name: 'instagram',
+    href: 'https://www.instagram.com',
+  },
+];
+export default socials;
