2ca3416e9c339987fae860e5c581158af76e5e8a
Jorge Marcos PC <desarrolladorjorgem@gmail.com>
2023-03-04 20:23:23 +0100

    feat: Creating widget Components

---
+++ components/widget/Navbar/index.ts
@@ -0,0 +1,32 @@
+import Navbar from './Navbar';
+export { default as MobileMenu } from './chunk/MobileMenu';
+export { default as NavItem } from './chunk/NavItem';
+export { default as OpenMenuBtn } from './chunk/OpenMenuBtn';
+export { default as SmNav } from './chunk/SmNav';
+export { OpenContext } from './Navbar';
+
+export default Navbar;
+
+export const menuOptions = [
+  {
+    id: 101,
+    text: '//01 <Inicio/>',
+    href: '#hero',
+  },
+  {
+    id: 102,
+    text: '//02 <SobreMi/>',
+    href: '#aboutme',
+  },
+  {
+    id: 103,
+    text: '//03 <MiPortfolio/>',
+    href: '#myportfolio',
+  },
+
+  {
+    id: 104,
+    text: '//04 <Contacto/>',
+    href: '#contact',
+  },
+];
