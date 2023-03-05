e49f683224d64290a1b44bc7f9034c963df275b6
Jorge Marcos PC <desarrolladorjorgem@gmail.com>
2023-03-05 12:00:49 +0100

    feat: Add the components and headers of the web

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,51 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+import Script from 'next/script';
+import { ReactElement } from 'react';
+import { GA_TRACKING_ID } from '../lib/helper/gtag';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+class MyDocument extends Document {
+  render(): ReactElement {
+    return (
+      <Html lang="es">
+        <Head>
+          <link rel="icon" href="/favicon.ico" />
+
+          <meta
+            name="description"
+            content="Yo soy Jorge Marcos. Desarrollador Fullstack de España. Me gusta jugar con Css. Mi Stack de desarrollo es Html, CSS, JS, TS, React, NEXT.JS, Tailwindcss, Scss."
+          />
+
+          <meta name="author" content="Jorge Marcos Carrero" />
+
+          <meta name="robots" content="index, follow" />
+
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <Script
+            strategy="afterInteractive"
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <Script
+            id="ga-script"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+          `,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,22 @@
 import { NextPage } from 'next';
 import Head from 'next/head';
-import { useEffect, useState } from 'react';
-import { About, Hero } from '../components/pages/Home';
+import Hero from '../components/pages/Home/Hero';
+import About from '../components/pages/Home/About';
+import Projects from '../components/pages/Home/Projects';
+import Contact from '../components/pages/Home/Contact';
 import Navbar from '../components/widget/Navbar';
 
 const Landing: NextPage = () => {
-  const [attention, setAttention] = useState(false);
-
-  useEffect(() => {
-    setTimeout(() => {
-      console.log('ds');
-      setAttention(true);
-    }, 15000);
-  }, []);
-
   return (
     <div className="relative min-h-screen overflow-x-hidden font-mono">
       <Head>
-        <title>Portfolio - Jorge Marcos Carrero</title>
+        <title>Portfolio - Jorge Marcos</title>
       </Head>
       <Navbar />
       <Hero />
       <About />
+      <Projects />
+      <Contact />
     </div>
   );
 };
